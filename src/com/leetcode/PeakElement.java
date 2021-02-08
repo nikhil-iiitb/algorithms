8ed94c6da36ee1410f03004abe3d30f40777d790
nikhil-iiitb <64706071+nikhil-iiitb@users.noreply.github.com>
2021-02-09 01:37:19 +0530

    Find Peak Element

---
+++ src/com/leetcode/PeakElement.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+//
+//  Find Peak Element
+//
+//  Created by Nikhil Garg on 09/02/2021.
+//  Copyright © 2021 Personal. All rights reserved.
+//
+
+public class PeakElement {
+
+    public int findPeakElement(int[] nums) {
+        if(null == nums || (nums.length-1==0))
+            return 0;
+
+        int low=0, high=nums.length-1;
+
+        while (low<high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] < nums[mid+1]) {
+                low = mid+1;
+            }else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        PeakElement pE = new PeakElement();
+        int[] inputArray = {1,2,1,3,5,6,4};
+        System.out.println(pE.findPeakElement(inputArray));
+    }
+
+}
