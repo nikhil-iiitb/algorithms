0cf6c91b67e34511451429a7e55e6e2265c98713
nikhil-iiitb <64706071+nikhil-iiitb@users.noreply.github.com>
2023-02-28 20:48:57 +0530

    Distribute Coins in Binary Tree

--- src/com/leetcode/TimeMap.java
+++ src/com/leetcode/TimeMap.java
@@ -41,3 +41,49 @@ public class TimeMap {
     }
 
 }
+
+/*
+Best Soln
+class TimeValue{
+    String val;
+    int timestamp;
+
+    public TimeValue(String val, int time){
+        this.val = val;
+        this.timestamp = time;
+    }
+}
+
+
+class TimeMap {
+    Map<String, List<TimeValue>> map;
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new ArrayList<>()).add(new TimeValue(value, timestamp));
+    }
+
+    public String get(String key, int timestamp) {
+        List<TimeValue> list = map.getOrDefault(key, null);
+        if(list == null){
+            return "";
+        }
+        int start = 0, end = list.size() - 1;
+
+        if(list.get(start).timestamp > timestamp) return  "";
+        if(list.get(end).timestamp <= timestamp) return list.get(end).val;
+
+        while(start <= end){
+            int mid = (start + end ) /2;
+            if(list.get(mid).timestamp == timestamp) return list.get(mid).val;
+            if(list.get(mid).timestamp < timestamp) start = mid + 1;
+            else end = mid - 1;
+        }
+        return list.get(start - 1).val;
+    }
+}
+
+
+ */
